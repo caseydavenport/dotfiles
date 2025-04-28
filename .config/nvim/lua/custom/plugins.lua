651e1048dacb3f6d9870a7e56de57511efc8e351
Casey Davenport <davenport.cas@gmail.com>
2025-04-28 09:12:34 -0700

    Add markdown preview

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -93,5 +93,11 @@ local plugins = {
     config = function(_)
     end
   },
+  {
+      "iamcco/markdown-preview.nvim",
+      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+      ft = { "markdown" },
+      build = ":call mkdp#util#install()",
+  },
 }
 return plugins
