9d41168d4b58a9c04acf31434238e21ec198db02
Casey Davenport <davenport.cas@gmail.com>
2026-03-19 12:26:26 -0700

    Rebind flash search

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -174,8 +174,8 @@ local plugins = {
     "folke/flash.nvim",
     event = "VeryLazy",
     keys = {
-      { "<CR>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash jump" },
-      { "<S-CR>", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash treesitter" },
+      { "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash jump" },
+      { "<leader>S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash treesitter" },
     },
     config = function()
       require("flash").setup()
